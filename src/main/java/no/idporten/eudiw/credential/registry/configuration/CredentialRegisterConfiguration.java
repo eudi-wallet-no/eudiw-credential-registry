193ef6f13304c2e4e44a25a0c8c392e4474321d5
Solveig Langbakk <solveig.langbakk@digdir.no>
2025-11-07 12:09:16 +0100

    sette ned read og connect timeout

--- src/main/java/no/idporten/eudiw/credential/registry/configuration/CredentialRegisterConfiguration.java
+++ src/main/java/no/idporten/eudiw/credential/registry/configuration/CredentialRegisterConfiguration.java
@@ -1,6 +1,5 @@
 package no.idporten.eudiw.credential.registry.configuration;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;

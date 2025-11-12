4d99b33c18b0fcd433dcb7a630c1e09b7115ec31
Solveig Langbakk <solveig.langbakk@digdir.no>
2025-11-12 09:29:10 +0100

    endre fra controller til restcontroller

--- src/main/java/no/idporten/eudiw/credential/registry/api/CredentialsController.java
+++ src/main/java/no/idporten/eudiw/credential/registry/api/CredentialsController.java
@@ -1,16 +1,14 @@
 package no.idporten.eudiw.credential.registry.api;
 
 import no.idporten.eudiw.credential.registry.response.CredentialRegisterService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import no.idporten.eudiw.credential.registry.response.model.Credentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CredentialsController {
 
     @Autowired
@@ -18,7 +16,6 @@ public class CredentialsController {
 
 
     @GetMapping(value= "/v1/credentials", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public Credentials credentials() {
         return credentialRegisterService.setResponse();
     }

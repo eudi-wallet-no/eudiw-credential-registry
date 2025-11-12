2ec9185e87d298af597060e3ac8eeeabec741ea9
Solveig Langbakk <solveig.langbakk@digdir.no>
2025-11-12 10:18:02 +0100

    fikse validering paa claims

--- src/main/java/no/idporten/eudiw/credential/registry/integration/model/Claims.java
+++ src/main/java/no/idporten/eudiw/credential/registry/integration/model/Claims.java
@@ -3,14 +3,15 @@ package no.idporten.eudiw.credential.registry.integration.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Claims(
-        @NotBlank
+        @NotNull
         @JsonProperty("path")
-        List<String> path,
+        List<@NotBlank String> path,
         @NotBlank
         @JsonProperty("display")
         List<Display> display

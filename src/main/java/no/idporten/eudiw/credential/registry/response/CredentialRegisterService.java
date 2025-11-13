01be93ae9dda4d5fc26fde9bfa7844a1fe2a6c4a
Solveig Langbakk <solveig.langbakk@digdir.no>
2025-11-13 13:57:03 +0100

    skrevet om til stream uttrykk

--- src/main/java/no/idporten/eudiw/credential/registry/response/CredentialRegisterService.java
+++ src/main/java/no/idporten/eudiw/credential/registry/response/CredentialRegisterService.java
@@ -7,7 +7,6 @@ import no.idporten.eudiw.credential.registry.response.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,7 +15,6 @@ public class CredentialRegisterService {
 
     private final CredentialIssuerMetadataRetriever credentialIssuerMetadataRetriever;
     private Credentials credentials;
-
     @Autowired
     public CredentialRegisterService(CredentialIssuerMetadataRetriever credentialIssuerMetadataRetriever) {
         this.credentialIssuerMetadataRetriever = credentialIssuerMetadataRetriever;
@@ -27,29 +25,16 @@ public class CredentialRegisterService {
         List<CredentialsIssuer> outputCredentials =
         credentialIssuerMetadataRetriever.getListOfIssuer().stream().flatMap((issuer) ->
             issuer.credentialConfiguration().entrySet().stream().map((key) ->
-                 inputDataToResponseIssuer(issuer.credentialIssuer(), key.getKey(), key.getValue(), key.getValue().credentialMetadata())
+                 inputDataToResponseIssuer(issuer.credentialIssuer(), key.getKey(), key.getValue())
             )
         ).toList();
         credentials = new Credentials(outputCredentials);
     }
 
-    private CredentialsIssuer inputDataToResponseIssuer(String issuer, String key, CredentialConfiguration credentialConfiguration, no.idporten.eudiw.credential.registry.integration.model.CredentialMetadata credentialMetadata) {
-        List<Claims> listOfClaims = new ArrayList<>();
-        List<Display> outerListOfDisplay = new ArrayList<>();
-        credentialMetadata.display().stream().forEach((display) -> {
-            Display displayLocal = new Display(display.name(), display.locale());
-            outerListOfDisplay.add(displayLocal);
-        });
-        credentialMetadata.claims().stream().forEach((claim) -> {
-            List<Display> innerListOfDisplay = new ArrayList<>();
-            claim.display().stream().forEach((display) -> {
-                Display displayLocal = new Display(display.name(), display.locale());
-                innerListOfDisplay.add(displayLocal);
-            });
-            Claims claims = new Claims(claim.path(), innerListOfDisplay);
-            listOfClaims.add(claims);
-        });
-        CredentialMetadata newCredentialMetadata = new CredentialMetadata(outerListOfDisplay, listOfClaims);
+    private CredentialsIssuer inputDataToResponseIssuer(String issuer, String key, CredentialConfiguration credentialConfiguration) {
+        List<Display> outerListOfDisplay = credentialConfiguration.credentialMetadata().display().stream().map(display -> new Display(display.name(), display.locale())).toList();
+        List<Claims> claimsList = credentialConfiguration.credentialMetadata().claims().stream().map(claims -> new Claims(claims.path(), claims.display().stream().map(display -> new Display(display.name(), display.locale())).toList())).toList();
+        CredentialMetadata newCredentialMetadata = new CredentialMetadata(outerListOfDisplay, claimsList);
         return new CredentialsIssuer(issuer, key, credentialConfiguration.doctype(), credentialConfiguration.format(), newCredentialMetadata);
     }
 

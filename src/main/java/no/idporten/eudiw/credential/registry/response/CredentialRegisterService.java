8e446acdcbb96b4d3fe401b2e302729e59592058
Solveig Langbakk <solveig.langbakk@digdir.no>
2025-11-13 11:39:33 +0100

    trakk ut metode for setting av respons issuer

--- src/main/java/no/idporten/eudiw/credential/registry/response/CredentialRegisterService.java
+++ src/main/java/no/idporten/eudiw/credential/registry/response/CredentialRegisterService.java
@@ -2,6 +2,7 @@ package no.idporten.eudiw.credential.registry.response;
 
 
 import no.idporten.eudiw.credential.registry.integration.CredentialIssuerMetadataRetriever;
+import no.idporten.eudiw.credential.registry.integration.model.CredentialConfiguration;
 import no.idporten.eudiw.credential.registry.response.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,28 +27,34 @@ public class CredentialRegisterService {
         List<CredentialsIssuer> outputCredentials = new ArrayList<>();
         credentialIssuerMetadataRetriever.getListOfIssuer().stream().forEach((issuer) -> {
             issuer.credentialConfiguration().entrySet().stream().forEach((key) -> {
-                List<Claims> listOfClaims = new ArrayList<>();
-                List<Display> outerListOfDisplay = new ArrayList<>();
-                key.getValue().credentialMetadata().display().stream().forEach((display) -> {
-                    Display displayLocal = new Display(display.name(), display.locale());
-                    outerListOfDisplay.add(displayLocal);
-                });
-                key.getValue().credentialMetadata().claims().stream().forEach((claim) -> {
-                    List<Display> innerListOfDisplay = new ArrayList<>();
-                    claim.display().stream().forEach((display) -> {
-                        Display displayLocal = new Display(display.name(), display.locale());
-                        innerListOfDisplay.add(displayLocal);
-                    });
-                    Claims claims = new Claims(claim.path(), innerListOfDisplay);
-                    listOfClaims.add(claims);
-                });
-                CredentialMetadata credentialMetadata = new CredentialMetadata(outerListOfDisplay, listOfClaims);
-                CredentialsIssuer treatedIssuer = new CredentialsIssuer(issuer.credentialIssuer(), key.getKey(), key.getValue().doctype(), key.getValue().format(), credentialMetadata);
-                outputCredentials.add(treatedIssuer);
+                CredentialsIssuer credentialsIssuer = inputDataToResponseIssuer(issuer.credentialIssuer(), key.getKey(), key.getValue(), key.getValue().credentialMetadata());
+
+                outputCredentials.add(credentialsIssuer);
             });
         });
         credentials = new Credentials(outputCredentials);
     }
+
+    private CredentialsIssuer inputDataToResponseIssuer(String issuer, String key, CredentialConfiguration credentialConfiguration, no.idporten.eudiw.credential.registry.integration.model.CredentialMetadata credentialMetadata) {
+        List<Claims> listOfClaims = new ArrayList<>();
+        List<Display> outerListOfDisplay = new ArrayList<>();
+        credentialMetadata.display().stream().forEach((display) -> {
+            Display displayLocal = new Display(display.name(), display.locale());
+            outerListOfDisplay.add(displayLocal);
+        });
+        credentialMetadata.claims().stream().forEach((claim) -> {
+            List<Display> innerListOfDisplay = new ArrayList<>();
+            claim.display().stream().forEach((display) -> {
+                Display displayLocal = new Display(display.name(), display.locale());
+                innerListOfDisplay.add(displayLocal);
+            });
+            Claims claims = new Claims(claim.path(), innerListOfDisplay);
+            listOfClaims.add(claims);
+        });
+        CredentialMetadata newCredentialMetadata = new CredentialMetadata(outerListOfDisplay, listOfClaims);
+        return new CredentialsIssuer(issuer, key, credentialConfiguration.doctype(), credentialConfiguration.format(), newCredentialMetadata);
+    }
+
     public Credentials  getCredentials() {
         return credentials;
     }

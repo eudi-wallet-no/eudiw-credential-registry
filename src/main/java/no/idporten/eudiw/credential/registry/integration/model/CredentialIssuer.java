b7dc769f326fe5defac3162a62d988c5fb8cbd11
Solveig Langbakk <solveig.langbakk@digdir.no>
2025-11-14 10:28:46 +0100

    notEmpty istedenfor notNull

--- src/main/java/no/idporten/eudiw/credential/registry/integration/model/CredentialIssuer.java
+++ src/main/java/no/idporten/eudiw/credential/registry/integration/model/CredentialIssuer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
@@ -19,7 +20,7 @@ public record CredentialIssuer(
         @NotNull
         @JsonProperty("credential_configurations_supported")
         Map<String, @Valid CredentialConfiguration> credentialConfiguration,
-        @NotNull
+        @NotEmpty
         @JsonProperty("display")
         List<@Valid Display> display
 ) {

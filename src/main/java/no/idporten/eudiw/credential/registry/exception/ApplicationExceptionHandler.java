7985b3961244c4b55b62612fe855b17500d4648d
solveiglangbakk <162119439+solveiglangbakk@users.noreply.github.com>
2026-01-08 15:29:56 +0100

    EUW-1058: Logging av issuer uri (#24)

---
+++ src/main/java/no/idporten/eudiw/credential/registry/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,21 @@
+package no.idporten.eudiw.credential.registry.exception;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationExceptionHandler.class);
+
+    @ExceptionHandler(CredentialRegisterException.class)
+    public ResponseEntity<ErrorResponse> credentialRegisterException(CredentialRegisterException credentialRegisterException) {
+        log.error("Credential Registry exception ", credentialRegisterException);
+        return ResponseEntity
+                .status(credentialRegisterException.getHttpStatus())
+                .body(new ErrorResponse(credentialRegisterException.getError(), credentialRegisterException.getErrorDescription()));
+    }
+}

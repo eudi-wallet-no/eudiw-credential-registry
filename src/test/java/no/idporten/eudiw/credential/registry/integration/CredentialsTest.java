b645e1a1c15486cd183e71ac4c5be087c65e69b8
Solveig Langbakk <solveig.langbakk@digdir.no>
2026-01-09 13:26:38 +0100

    legge til https test og issuer uri tom test

--- src/test/java/no/idporten/eudiw/credential/registry/integration/CredentialsTest.java
+++ src/test/java/no/idporten/eudiw/credential/registry/integration/CredentialsTest.java
@@ -1,15 +1,19 @@
 package no.idporten.eudiw.credential.registry.integration;
 
 
+import no.idporten.eudiw.credential.registry.integration.model.CredentialIssuer;
 import no.idporten.eudiw.credential.registry.response.CredentialRegisterService;
 import no.idporten.eudiw.credential.registry.response.model.Credentials;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.net.URI;
 
 @DisplayName("credentials testing")
 @SpringBootTest
@@ -40,5 +44,24 @@ public class CredentialsTest {
                 () -> assertEquals("mdoc", credentials.credentials().get(0).format()),
                 () -> assertEquals("sd+jwt-vc", credentials.credentials().get(1).format())
         );
+    }
+
+    @DisplayName("When issuer URL is not https")
+    @Test
+    void whenIssuerUrlIsNotHttps() {
+        this.credentialRegisterService = new CredentialRegisterService(mockRetriever);
+        URI uri = URI.create("http://localhost:8080/");
+        CredentialIssuer issuer = mockRetriever.fetchCredentialIssuerFromMetadataRequest(uri);
+
+        assertNull(issuer);
+    }
+
+    @DisplayName("When issuer uri is empty")
+    @Test
+    void whenIssuerUriIsEmpty() {
+        this.credentialRegisterService = new CredentialRegisterService(mockRetriever);
+        URI uri = URI.create("");
+        CredentialIssuer issuer = mockRetriever.fetchCredentialIssuerFromMetadataRequest(uri);
+        assertNull(issuer);
     }
 }
